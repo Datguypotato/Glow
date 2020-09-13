e287ccc7f04861ddfff09de50b957448a9393440
Gordon home PC <aardappel156@gmail.com>
2020-09-13 17:47:18 +0200

    Created a local version

---
+++ GlowPlatformer/Assets/Scripts/Player/LocalPlayer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalPlayer : MonoBehaviour
+{
+    public KeyCode up;
+    public KeyCode down;
+    public KeyCode left;
+    public KeyCode right;
+
+    [SerializeField] float m_Speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(up))
+        {
+            transform.Translate(Vector3.up * m_Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(down))
+        {
+            transform.Translate(Vector3.down * m_Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(left))
+        {
+            transform.Translate(Vector3.left * m_Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(right))
+        {
+            transform.Translate(Vector3.right * m_Speed * Time.deltaTime);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log(gameObject.name + " has been clicked");
+        LocalPlayerManager.instance.UpdateInputField(this);
+    }
+
+}
---
+++ GlowPlatformer/Assets/Scripts/Player/LocalPlayerManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class LocalPlayerManager : MonoBehaviour
+{
+    public static LocalPlayerManager instance;
+
+    [SerializeField] private GameObject m_localPlayerPrefab;
+    [SerializeField] private Transform m_PlayerHolder;
+    [SerializeField] private LocalPlayer m_CurrFocusedPlayer;
+
+    [SerializeField] private TMP_InputField up;
+    [SerializeField] private TMP_InputField down;
+    [SerializeField] private TMP_InputField left;
+    [SerializeField] private TMP_InputField right;
+
+    private void Awake()
+    {
+        if(instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    public void SpawnPlayer()
+    {
+        Debug.Log("spawned player");
+        Instantiate(m_localPlayerPrefab, m_PlayerHolder);
+    }
+
+    public void UpdateInputField(LocalPlayer a_localPlayer)
+    {
+        m_CurrFocusedPlayer = a_localPlayer;
+
+        up.text = a_localPlayer.up.ToString();
+        down.text = a_localPlayer.down.ToString();
+        left.text = a_localPlayer.left.ToString();
+        right.text = a_localPlayer.right.ToString();
+    }
+
+    public void UpdatePlayerInputs()
+    {
+        m_CurrFocusedPlayer.up = (KeyCode)System.Enum.Parse(typeof(KeyCode), up.text.ToUpper());
+        m_CurrFocusedPlayer.down = (KeyCode)System.Enum.Parse(typeof(KeyCode), down.text.ToUpper());
+        m_CurrFocusedPlayer.left = (KeyCode)System.Enum.Parse(typeof(KeyCode), left.text.ToUpper());
+        m_CurrFocusedPlayer.right = (KeyCode)System.Enum.Parse(typeof(KeyCode), right.text.ToUpper());
+
+        Debug.Log("Updated " + m_CurrFocusedPlayer.name + " inputs");
+    }
+}
